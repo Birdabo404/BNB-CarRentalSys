d048d6cab11b67eae426b6869d51baecb7f2eea4
Birdabo404 <birdabo420@gmail.com>
2025-04-28 10:45:03 +0800

    working on Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Navbar from './components/ui/Navbar/Navbar.jsx'
+import { MouseTrail } from '@stichiboi/react-elegant-mouse-trail'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <> 
+      <MouseTrail strokeColor="#F97316" lineWidthStart={30} />
+      <Navbar>
+        
+      </Navbar>
+    </>
   )
 }
 
---
+++ frontend/src/components/ui/Navbar/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logoImage from '../../../assets/images/logo-rev1.png'
+
+const Logo = () => {
+  return (
+    <div className="logo h-21 w-21">
+        <img src={logoImage} alt="LOGO_AINT_WORKING" />
+    </div>
+  )
+}
+
+export default Logo
